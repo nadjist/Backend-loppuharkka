4eb456b5f0f2a0b119c3460c19b749e4395103a5
nadjis <nadja.gronfors@gmail.com>
2023-05-27 16:16:41 +0300

    added error handling to PlayerById.

--- getPlayerById.py
+++ getPlayerById.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from database import players
 
 router = APIRouter()
@@ -6,5 +6,6 @@ router = APIRouter()
 
 @router.get('/players/{id}')
 def getPlayerById(id: int):
-
+    if id not in players:
+        raise HTTPException(status_code=404, message="Tuntematon pelaaja")
     return players[id]
